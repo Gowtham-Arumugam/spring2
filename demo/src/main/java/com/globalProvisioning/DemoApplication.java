dd7b2a1685d07858c0acbd4f1cbcc9581093aeac
agowt <agowt@Lenovo>
2024-06-25 11:55:48 +0530

    changed second time

--- demo/src/main/java/com/globalProvisioning/DemoApplication.java
+++ demo/src/main/java/com/globalProvisioning/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	//This is demo application
+	//need to find why its now working
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
